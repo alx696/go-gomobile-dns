c0f6aebebcc3b6738d7b3866a89b0ca25621a472
李小明 <likeming@jasgroup.cn>
2020-06-01 16:03:47 +0800

    增加路径提示.

--- aar.sh
+++ aar.sh
@@ -5,7 +5,7 @@ TARGET_DIR=""
 if [ -n "$1" ]; then
   TARGET_DIR=$1
 else
-  read -p "设置输出文件夹，按Enter键:" CHOOSE
+  read -p "设置输出文件夹(末尾不带斜杠)，按Enter键:" CHOOSE
   TARGET_DIR=$CHOOSE
 fi
 echo "输出文件夹：${TARGET_DIR}"
@@ -14,14 +14,14 @@ echo "输出文件夹：${TARGET_DIR}"
 go mod vendor; cp -r vendor/* $GOPATH/src/; rm -rf $GOPATH/src/pkg $GOPATH/src/modules.txt ; rm -rf vendor
 
 # 复制源码
-TEMPPATH="$GOPATH/src/lilu.red/temp"
+TEMPPATH="$GOPATH/src/lilu.red/aar"
 mkdir -p $TEMPPATH
 # dns包
 cp -r dns $TEMPPATH
 
 # 打包
 GO111MODULE="off"
-gomobile bind -v -o "${TARGET_DIR}gomobile.aar" -target=android "${TEMPPATH}/dns"
+gomobile bind -v -o "${TARGET_DIR}/gomobile.aar" -target=android "${TEMPPATH}/dns"
 rm -rf TEMPPATH
 
 echo "打包完成"
